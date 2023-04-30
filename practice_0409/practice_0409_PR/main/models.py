63468e840051a084ec4f0cdc2c9754155edfb5a3
senghoho <qisstun@naver.com>
2023-04-30 16:17:51 +0900

    commit_0430

--- practice_0409/practice_0409_PR/main/models.py
+++ practice_0409/practice_0409_PR/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    progress = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:20]
